fcdf4add80fab19afd5a75907b75257cebb19384
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2019-11-25 23:19:44 -0500

    first commit

---
+++ SudokuSolver/test.py
@@ -0,0 +1,84 @@
+from filecmp import cmp
+
+from utils import *
+
+
+# `grid` is defined in the test code scope as the following:
+# (note: changing the value here will _not_ change the test code)
+# grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    dict = {}
+    for index, box in enumerate(boxes):
+        if grid[index] == '.':
+            dict[box] = '123456789'
+        else:
+            dict[box] = grid[index]
+    return dict
+
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    for position in values:
+        if len(values[position]) == 1:
+            my_peers = peers[position]
+            for peer in my_peers:
+                removed = values[peer].replace(values[position], "")
+                values[peer] = removed
+    return values
+
+
+def eliminateCorrect(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit, '')
+    return values
+
+
+def main():
+    print("Hello World!")
+    dict = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+    display(dict)
+    myDict1 = eliminate(dict)
+    myDict2 = eliminateCorrect(dict)
+
+    print(myDict1 == myDict2)
+
+
+# row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
+# column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
+# square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+
+
+if __name__ == "__main__":
+    main()
