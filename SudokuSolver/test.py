07b89112e0bbfda9239fe4524ecda56dbc993ce3
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2019-12-01 22:37:50 -0500

    finished eliminated method

--- SudokuSolver/test.py
+++ SudokuSolver/test.py
@@ -1,28 +1,38 @@
-from typing import Dict
+from typing import Dict, List, Any, Set
 
 from utils import *
 
 
 def grid_values(grid: str) -> dict:
-    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
-
-    Args:
-        grid: Sudoku grid in string form, 81 characters long
-    Returns:
-        Sudoku grid in dictionary form:
-        - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
-    """
     dictBoxes: Dict[str, str] = {}
     for index, box in enumerate(boxes):
-        dictBoxes[box] = grid[index]
+        if grid[index] == ".":
+            dictBoxes[box] = "123456789"
+        else:
+            dictBoxes[box] = grid[index]
     return dictBoxes
+
+
+def eliminate(values: Dict) -> Dict:
+    singleValueBoxLocations = set()
+
+    for boxLocation, boxValuePossibilities in values.items():
+        if len(boxValuePossibilities) == 1:
+            singleValueBoxLocations.add(boxLocation)
+
+    for boxLocation in singleValueBoxLocations:
+        currPeers: Set[str] = peers[boxLocation]
+        for peer in currPeers:
+            values[peer] = values[peer].replace(values[boxLocation], '')
+
+    return values
 
 
 def main():
     print("Hello World!")
     dictBoxes: dict = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
-    print(dictBoxes)
+    dictBoxes = eliminate(dictBoxes)
+    display(dictBoxes)
 
 
 # boxes = ['A1', 'A2', ..., 'I9']
