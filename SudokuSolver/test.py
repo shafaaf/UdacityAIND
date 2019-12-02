f7f60049c22a63bc2fa1d0962ca126b2663ee3dc
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2019-12-01 21:49:47 -0500

    rewriting with types

--- SudokuSolver/test.py
+++ SudokuSolver/test.py
@@ -1,11 +1,9 @@
+from typing import Dict
+
 from utils import *
 
 
-# `grid` is defined in the test code scope as the following:
-# (note: changing the value here will _not_ change the test code)
-# grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
-
-def grid_values(grid):
+def grid_values(grid: str) -> dict:
     """Convert grid string into {<box>: <value>} dict with '.' value for empties.
 
     Args:
@@ -15,45 +13,22 @@ def grid_values(grid):
         - keys: Box labels, e.g. 'A1'
         - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
     """
-    dict = {}
+    dictBoxes: Dict[str, str] = {}
     for index, box in enumerate(boxes):
-        if grid[index] == '.':
-            dict[box] = '123456789'
-        else:
-            dict[box] = grid[index]
-    return dict
-
-
-def eliminate(values):
-    """Eliminate values from peers of each box with a single value.
-
-    Go through all the boxes, and whenever there is a box with a single value,
-    eliminate this value from the set of values of all its peers.
-
-    Args:
-        values: Sudoku in dictionary form.
-    Returns:
-        Resulting Sudoku in dictionary form after eliminating values.
-    """
-    solved_values = [box for box in values.keys() if len(values[box]) == 1]
-    for box in solved_values:
-        digit = values[box]
-        for peer in peers[box]:
-            values[peer] = values[peer].replace(digit, '')
-    return values
+        dictBoxes[box] = grid[index]
+    return dictBoxes
 
 
 def main():
     print("Hello World!")
-    dict = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
-    display(dict)
-    eliminate(dict)
+    dictBoxes: dict = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+    print(dictBoxes)
 
 
+# boxes = ['A1', 'A2', ..., 'I9']
 # row_units[0] = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
 # column_units[0] = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
-# square_units[0] = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-
+# square_units[0] = ['A1', 'A2', 'A
 
 if __name__ == "__main__":
     main()
